ec42eb3ccd2669154468a909fae026a72e0e06f1
Ife Odugbesan <ifeodugbesan@yahoo.com>
2022-05-04 21:55:17 +0100

    added download function

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,3 +14,20 @@ ActiveStorage.start()
 
 import "controllers"
 import "bootstrap"
+import domtoimage from 'dom-to-image';
+import { saveAs } from 'file-saver';
+
+const btn = document.getElementById('downloadButton')
+
+var node = document.getElementById('completedPost');
+
+btn.addEventListener('click', (event) => {
+  const scale = 1;
+  domtoimage.toPng((node), { quality: 1, width: node.clientWidth * scale,  height: node.clientHeight * scale,  style: {   transform: 'scale('+scale+')',   transformOrigin: 'top left' } })
+    .then(function (dataUrl) {
+        var link = document.createElement('a');
+        link.download = 'matchday-post.png';
+        link.href = dataUrl;
+        link.click();
+    });
+})
