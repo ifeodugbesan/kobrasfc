3608546e3f16ffab6946cd0eb05a4d89e8275d44
Ife Odugbesan <ifeodugbesan@yahoo.com>
2022-05-04 22:32:10 +0100

    testing with append child

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,17 +17,29 @@ import "bootstrap"
 import domtoimage from 'dom-to-image';
 import { saveAs } from 'file-saver';
 
+
 const btn = document.getElementById('downloadButton')
 
 var node = document.getElementById('completedPost');
 
 btn.addEventListener('click', (event) => {
-  const scale = 1;
-  domtoimage.toPng((node), { quality: 1, width: node.clientWidth * scale,  height: node.clientHeight * scale,  style: {   transform: 'scale('+scale+')',   transformOrigin: 'top left' } })
+
+  domtoimage.toPng(node)
     .then(function (dataUrl) {
-        var link = document.createElement('a');
-        link.download = 'matchday-post.png';
-        link.href = dataUrl;
-        link.click();
+        var img = new Image();
+        img.src = dataUrl;
+        document.body.appendChild(img);
+    })
+    .catch(function (error) {
+        console.error('oops, something went wrong!', error);
     });
+
+  // const scale = 1;
+  // domtoimage.toPng((node), { quality: 1, width: node.clientWidth * scale,  height: node.clientHeight * scale,  style: {   transform: 'scale('+scale+')',   transformOrigin: 'top left' } })
+  //   .then(function (dataUrl) {
+  //       var link = document.createElement('a');
+  //       link.download = 'matchday-post.png';
+  //       link.href = dataUrl;
+  //       link.click();
+  //   });
 })
